34fd060ec08656cc83f43b8a9407c694ed2ad31a
ocavue <ocavue@gmail.com>
2020-12-21 20:19:06 +0800

    add comment

--- test/e2e.spec.ts
+++ test/e2e.spec.ts
@@ -1,6 +1,6 @@
 describe("e2e test", () => {
     test("main", async () => {
-        await page.goto("http://localhost:8081")
+        await page.goto("http://localhost:8081") // Run `yarn run dev` to start the webpack-dev-server firstly.
         await page.waitForSelector('#math-result')
         let text = await page.$eval('#math-result', (el) => el.innerHTML)
         expect(text).toContain("100")
