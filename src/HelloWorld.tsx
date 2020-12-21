f69b9bc224f1104beab10c57c3960b931bc815b5
ocavue <ocavue@gmail.com>
2020-12-21 20:07:38 +0800

    add two functions

--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 
+function doSomeMath1() {
+    let sum = 0
+    for (let i = 0; i < 100; i++) {
+        sum += 1
+    }
+    return sum
+}
+
+
+function doSomeMath2() {
+    let sum = 0
+    for (let i = 0; i < 100; i++) {
+        sum += 2
+    }
+    return sum
+}
+
 const HelloWorld = () => {
+    let sum = 0
+    if (1+1 == 2) {
+        sum = doSomeMath1()
+    } else {
+        sum = doSomeMath2()
+    }
+
     return (
         <div className='hello-world'>
             <h1>Hello World</h1>
-            <p>Welcome to my world</p>
+            <p>The math result is {sum}</p>
         </div>
     )
 }
